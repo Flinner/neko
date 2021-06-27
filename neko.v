62cbb925b3ed922963ba4732920ac87329167e91
Flinner <flinner.hlqn@aleeas.com>
2021-06-27 19:36:35 +0300

    feat: stdin

--- neko.v
+++ neko.v
@@ -6,18 +6,17 @@ fn main() {
 	// handle args might exit on errors(1) or help(0) exitcode=(n)
 	// filenames: []string, settings Settings
 	filenames, settings := handle_args()
+	mut content := ''
 
 	for filename in filenames {
 		if filename == '-' {
-			println('TODO: stdin')
+			content = os.get_raw_lines_joined()
 		} else {
-			content := os.read_file(filename) or { panic('TODO: msg:  no such file!') }
-
-			uwued := uwu(content)
-			formated := format(uwued, settings)
-
-			println('$formated')
+			content = os.read_file(filename) or { panic('No Such File; $filename') }
 		}
+		uwued := uwu(content)
+		formated := format(uwued, settings)
+
+		println('$formated')
 	}
-	println('$settings')
 }
