a8a2ab085a2458504c47567b614007646f563a02
Flinner <flinner.hlqn@aleeas.com>
2021-06-27 15:56:55 +0300

    feat: basic args and settings

--- args.v
+++ args.v
@@ -1,8 +1,42 @@
 module main
 
 import os
+import os.cmdline
 
-fn handle_args() []string {
-	args := os.args.clone()
-	return args
+const all_options = [
+	'-b',
+	'-e',
+	'-n',
+	'-s',
+	'-t',
+	'-u',
+	'-h',
+	'-v',
+]
+
+fn handle_args() ([]string, Settings) {
+	mut settings := Settings{}
+
+	args := os.args.clone()[1..] // raw args
+	filenames := args.filter(!(it.starts_with('-') && it.bytes().len > 1)) // filenames, includes '-' (stdin)
+	options := cmdline.only_options(args).filter(it != '-') // options, not including '-'
+
+	println('options: $options\nfilename: $filenames')
+
+	// check for correct options
+	for option in options {
+		if option in all_options {
+			continue
+		} else {
+			panic('Unkown option $option')
+		}
+	}
+
+	// help
+	if '-h' in options {
+		println('help message: TODO!')
+		exit(0)
+	}
+
+	return filenames, settings
 }
--- neko.v
+++ neko.v
@@ -2,15 +2,24 @@ module main
 
 import os
 
-fn main() {
-	filenames := handle_args()
-
-	filename := filenames[1]
-	content := os.read_file(filename) or { panic('bruh no such file!') }
-
-	uwu_content := uwu(content)
-
-	println('$filename:\n $uwu_content')
+struct Settings {
+pub mut:
+	number_non_blank_lines bool // -b Number the lines, but don't count blank lines
+	print_dollar           bool //-e Print a dollar sign (‘$’) at the end of each line. Implies the -v option
+	number_all             bool //-n Number the output lines, starting at 1.
+	squeze_blank_lines     bool //-s Squeeze multiple adjacent empty lines, causing the output to be single spaced.
+	print_tab              bool //-t Print tab characters as ‘^I’. Implies the -v option to display non-printing characters.
+	unbuffered             bool //-u  The output is guaranteed to be unbuffered
+	non_priting            bool //-v Displays non-printing characters so they are visible.
 }
 
-// uwufy text
+fn main() {
+	// handle args might exit on errors(1) or help(0) exitcode=(n)
+	filenames, settings := handle_args()
+
+	for filename in filenames {
+		content := os.read_file(filename) or { panic('bruh no such file!') }
+		println('${uwu(content)}')
+	}
+	println('$settings')
+}
